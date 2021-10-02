0d840da2f3e3295d32f45f5113868acd7d1889e5
Avishkar <gandhiavishkar@gmail.com>
2021-10-02 23:38:17 +0530

    Array I/P,O/P and Binary Search
    Binary Search: Search a sorted array by repeatedly dividing the search interval in half. Begin with an interval covering the whole array. If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half. Otherwise, narrow it to the upper half. Repeatedly check until the value is found or the interval is empty.

---
+++ Java/BinarySearch.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,0,8,9,6};
+        int tofindtarget = 9;
+        int targetindex = binarySearch(arr, tofindtarget);
+        System.out.print("Element found at index position: ");
+        System.out.println(targetindex);
+    }
+
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
