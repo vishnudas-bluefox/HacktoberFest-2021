58df41fd3f3cf9d3f78f550713758cb6acabde5d
Abhinandan Kumar <54628250+95871abhinandankumar@users.noreply.github.com>
2021-10-02 22:43:23 +0530

    Updated the code
    Reduced the time complexity

--- C/fibonacci.c
+++ C/fibonacci.c
@@ -1,17 +1,27 @@
-// 0 1 1 2 3 5 8 13
-
-#include <stdio.h>
-
+#include<stdio.h>
+ 
 int fib(int n)
 {
-  if(n<=1)
-  {
-    return n;
-  }
-  return fib(n-2)+fib(n-1);
-}
 
-int main(void) {
-  printf("%d",fib(5));
+  int f[n+2];   // 1 extra to handle case, n = 0
+  int i;
+
+  f[0] = 0;
+  f[1] = 1;
+ 
+  for (i = 2; i <= n; i++)
+  {
+
+      f[i] = f[i-1] + f[i-2];
+  }
+ 
+  return f[n];
+}
+ 
+int main ()
+{
+  int n = 9;
+  printf("%d", fib(n));
+  getchar();
   return 0;
 }
